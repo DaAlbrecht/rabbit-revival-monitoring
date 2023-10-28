f9ab8fd9c144799dd2cfb60c6d7d107bdd89dc0c
dominika <dominik.albrecht@intersim.ch>
2023-10-28 10:59:26 +0200

    Style table overflow, style scrollbar

--- src/routes/Messages/Messages.svelte
+++ src/routes/Messages/Messages.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <div
-  class="dark:highlight-white/5 relative h-[calc(100%-250px)] rounded-lg border-2 border-slate-50 dark:border-slate-400"
+  class="dark:highlight-white/5 relative h-[calc(100%-250px)] overflow-hidden rounded-lg border-2 border-slate-50 dark:border-slate-400"
 >
   <div class="flex h-full flex-col">
     <div class="flex items-center justify-between p-4">
@@ -26,39 +26,30 @@
         Messages
       </p>
     </div>
-    <div class="relative flex max-h-full grow flex-col p-4">
-      <table class="mt-2 max-h-full w-full overflow-y-scroll whitespace-nowrap text-left">
-        <colgroup>
-          <col class="w-1/12" />
-          <col class="w-3/12" />
-          <col class="w-4/12" />
-          <col class="w-1/12" />
-          <col class="w-2/12" />
-          <col class="w-1/12" />
-        </colgroup>
-        <thead class="border-b border-white/10 text-sm leading-6 text-white">
-          <tr>
-            <th scope="col" class="hidden py-2 pl-0 pr-8 font-semibold sm:table-cell">Offset</th>
-            <th
-              scope="col"
-              class="py-2 pl-0 pr-4 text-right font-semibold sm:pr-8 sm:text-left lg:pr-20"
+    <div class="relative flex h-[calc(100%-58px)] grow flex-col p-4">
+      <table class="mt-2 flex h-full w-full flex-col whitespace-nowrap text-left dark:bg-slate-900">
+        <thead
+          class="w-[calc(100%-0.9em)] shrink-0 grow-0 basis-auto border-b border-white/10 text-sm leading-6 text-white"
+        >
+          <tr class="table w-full table-fixed">
+            <th scope="col" class="w-1/12 py-2 font-semibold sm:table-cell">Offset</th>
+            <th scope="col" class="w-3/12 py-2 text-right font-semibold sm:pr-8 sm:text-left"
               >Transaction</th
             >
-            <th
-              scope="col"
-              class="py-2 pl-0 pr-4 text-right font-semibold sm:pr-8 sm:text-left lg:pr-20">Data</th
+            <th scope="col" class="w-4/12 py-2 text-right font-semibold sm:pr-8 sm:text-left"
+              >Data</th
             >
-            <th scope="col" class="hidden py-2 pl-0 pr-8 font-semibold sm:table-cell">Status</th>
-            <th scope="col" class="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20"
-              >Timestamp</th
-            >
-            <th scope="col" class="hidden">Actions</th>
+            <th scope="col" class="w-1/12 py-2 font-semibold sm:table-cell">Status</th>
+            <th scope="col" class="w-2/12 py-2 font-semibold md:table-cell">Timestamp</th>
+            <th scope="col" class="w-1/12">Actions</th>
           </tr>
         </thead>
-        <tbody class="divide-y divide-white/5">
+        <tbody
+          class="grow-1 shrink-1 scrollbar-styled block basis-auto divide-y divide-white/5 overflow-y-scroll"
+        >
           {#each Array(50) as _, i}
-            <tr>
-              <td class="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8">
+            <tr class="table w-full table-fixed">
+              <td class="w-1/12 py-4 pr-4 sm:pr-8">
                 <div class="flex gap-x-3">
                   <div
                     class="rounded-md bg-gray-700/40 px-2 py-1 text-xs font-medium text-gray-400 ring-1 ring-inset ring-white/10"
@@ -67,15 +58,11 @@
                   </div>
                 </div>
               </td>
-              <td
-                class="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20"
+              <td class="w-3/12 py-4 pr-8 text-sm leading-6 text-gray-400"
                 >{items[0].transaction.value}</td
               >
-              <td
-                class="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20"
-                >{items[0].data}</td
-              >
-              <td class="py-4 pl-0 pr-4 text-sm leading-6 sm:pr-8 lg:pr-20">
+              <td class="w-4/12 py-4 pr-8 text-sm leading-6 text-gray-400">{items[0].data}</td>
+              <td class="w-1/12 py-4 pr-4 text-sm leading-6 sm:pr-8">
                 <div class="flex items-center justify-end gap-x-2 sm:justify-start">
                   <div class={statuses[items[0].status] + ' flex-none rounded-full p-1'}>
                     <div class="h-2 w-2 rounded-full bg-current" />
@@ -83,12 +70,8 @@
                   <div class="hidden text-white sm:block">{items[0].status}</div>
                 </div>
               </td>
-              <td
-                class="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20"
-                >{items[0].timestamp}</td
-              >
-              <td
-                class="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20"
+              <td class="w-2/12 py-4 pr-8 text-sm leading-6 text-gray-400">{items[0].timestamp}</td>
+              <td class="w-1/12 py-4 pr-8 text-sm leading-6 text-gray-400"
                 ><a
                   href="#"
                   class="rounded bg-rose-400 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-500"
