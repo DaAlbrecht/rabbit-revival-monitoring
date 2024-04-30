23b53cfd94f2e3991bc89102599969105bb13789
DaAlbrecht <dadidi42@gmail.com>
2024-04-30 21:00:17 +0200

    feat: use dyn env variables

--- src/routes/api/fetch.ts
+++ src/routes/api/fetch.ts
@@ -1,18 +1,20 @@
-import { RABBIT_PASSWORD, RABBIT_USERNAME } from '$env/static/private';
-import { env } from '$env/dynamic/public';
+import { env } from '$env/dynamic/private';
+import { PUBLIC_RABBIT_MANAGEMENT_URL } from '$env/static/public';
 import { error } from '@sveltejs/kit';
 
 export const rabbitFetch = async (endpoint: string) => {
-  const url = `${env.PUBLIC_RABBIT_MANAGEMENT_URL}/api/${endpoint}`;
-  try {
-    const res = await fetch(url, {
-      headers: {
-        Authorization: `Basic ${btoa(`${RABBIT_USERNAME}:${RABBIT_PASSWORD}`)}`
-      }
-    });
-    const json = await res.json();
-    return json;
-  } catch (e) {
-    throw error(500, 'RabbitMQ API error');
-  }
+    const RABBIT_USERNAME = env.RABBIT_USERNAME;
+    const RABBIT_PASSWORD = env.RABBIT_PASSWORD;
+    const url = `${PUBLIC_RABBIT_MANAGEMENT_URL}/api/${endpoint}`;
+    try {
+        const res = await fetch(url, {
+            headers: {
+                Authorization: `Basic ${btoa(`${RABBIT_USERNAME}:${RABBIT_PASSWORD}`)}`
+            }
+        });
+        const json = await res.json();
+        return json;
+    } catch (e) {
+        throw error(500, 'RabbitMQ API error');
+    }
 };
